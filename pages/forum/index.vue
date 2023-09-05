7af31e6eba1c137dd970dd05edecc92093204e53
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-05 17:35:52 +0200

    styling

--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -1,20 +1,25 @@
 <template>
-  <Container>
-    <a v-for="thread in threadsMeta" :key="thread.id" :href="`/forum/${thread.id}`">
-      <div class="bg-gray-100 p-4 rounded-lg mb-6">
-        <h2 class="text-xl font-semibold">{{ thread.title }}</h2>
-      </div>
-    </a>
-  </Container>
+    <Container>
+        <div class="pt-8">
+            <h1 class="mb-4 text-3xl font-bold text-gray-800 md:text-7xl dark:text-white text-center"> Genie Discord forum
+            </h1>
+            <div class="space-y-4">
+                <a v-for="thread in threadsMeta" :key="thread.id" :href="`/forum/${thread.id}`"
+                    class="block bg-gray-100 mt-1 p-2 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 hover:bg-gray-200">
+                    <h2 class="text-lg font-medium  border-gray-300  flex items-center">{{ thread.title }}</h2>
+                </a>
+            </div>
+        </div>
+    </Container>
 </template>
 
 <script>
 import threadsMeta from '~/static/threads-meta.json'
 export default {
-  data() {
-    return {
-      threadsMeta
+    data() {
+        return {
+            threadsMeta
+        }
     }
-  }
 }
 </script>
