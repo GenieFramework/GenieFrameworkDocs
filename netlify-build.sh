338e09fdce1c74804edfa3c508b03065d238f9f6
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-05 16:46:18 +0200

    fix curl

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Downloading forum files"
-curl -L -o forum.tar.gz https://www.dropbox.com/scl/fi/sm74dpgkd0yor50heeu8w/forum.tar.gz?rlkey=fedrbtvdnvzb7wmn8wu16al50 &
-dl=1
+curl -L -o forum.tar.gz https://www.dropbox.com/scl/fi/sm74dpgkd0yor50heeu8w/forum.tar.gz?rlkey=fedrbtvdnvzb7wmn8wu16al50&dl=1
 
 tar -xzvf forum.tar.gz -C static/
 
