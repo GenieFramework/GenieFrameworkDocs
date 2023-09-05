f1e01a44830c8d29904752e8961073f82e5365d6
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-05 16:41:43 +0200

    debug msgs

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+echo "Downloading forum files"
 curl -L -o forum.tar.gz https://www.dropbox.com/scl/fi/sm74dpgkd0yor50heeu8w/forum.tar.gz?rlkey=fedrbtvdnvzb7wmn8wu16al50 &
 dl=1
 
 tar -xzvf forum.tar.gz -C static/
-# npm run build
+
+echo "Building site"
+npm run build
