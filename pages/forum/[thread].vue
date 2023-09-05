fa8d309b7d4c9255c16fd89b280dcf57696871ac
Mohamed Sayed <ms10596@gmail.com>
2023-09-05 09:52:24 +0300

    forum: dynamic import

--- pages/forum/[thread].vue
+++ pages/forum/[thread].vue
@@ -1,64 +1,61 @@
 <template>
-    <Container>
-        <div v-if="thread && thread.guild" class="mx-auto max-w-5xl mt-10">
-            <div class="bg-gray-100 p-4 rounded-lg mb-6">
-                <a :href="`https://discord.com/channels/${thread.guild.id}/${thread.channel.id}`">
-                    <h2 class="text-xl font-semibold">{{ thread.channel.name }}</h2>
-                    <p v-if="thread.channel.topic" class="text-gray-600 mt-2">{{ thread.channel.topic }}</p>
-                </a>
-            </div>
+  <Container>
+    <div v-if="thread && thread.guild" class="mx-auto max-w-5xl mt-10">
+      <div class="bg-gray-100 p-4 rounded-lg mb-6">
+        <a :href="`https://discord.com/channels/${thread.guild.id}/${thread.channel.id}`">
+          <h2 class="text-xl font-semibold">{{ thread.channel.name }}</h2>
+          <p v-if="thread.channel.topic" class="text-gray-600 mt-2">{{ thread.channel.topic }}</p>
+        </a>
+      </div>
 
-            <div class="space-y-6">
-                <div v-for="message in parsedMessages" :key="message.id" class="flex space-x-6">
-                    <div class="flex-1 bg-white p-4 rounded-lg shadow mb-4">
-                        <a :href="`https://discord.com/channels/${thread.guild.id}/${thread.channel.id}/${message.id}`"
-                            target="_blank" class="block hover:underline">
-                            <div class="flex justify-between items-center mb-2">
-                                <div class="font-medium flex items-center space-x-2"
-                                    :style="{ color: message.author.color }">
-                                    <img :src="message.author.avatarUrl" alt="Author Avatar"
-                                        class="w-10 h-10 rounded-full" />
-                                    <span class="ml-2"> {{ message.author.nickname || message.author.name }}</span>
-                                </div>
-                                <span class="text-gray-500 text-sm">{{ new Date(message.timestamp).toLocaleString()
-                                }}</span>
-                            </div>
-                        </a>
-                        <ContentRendererMarkdown :value="message.parsedContent" />
-                    </div>
+      <div class="space-y-6">
+        <div v-for="message in parsedMessages" :key="message.id" class="flex space-x-6">
+          <div class="flex-1 bg-white p-4 rounded-lg shadow mb-4">
+            <a :href="`https://discord.com/channels/${thread.guild.id}/${thread.channel.id}/${message.id}`"
+              target="_blank" class="block hover:underline">
+              <div class="flex justify-between items-center mb-2">
+                <div class="font-medium flex items-center space-x-2" :style="{ color: message.author.color }">
+                  <img :src="message.author.avatarUrl" alt="Author Avatar" class="w-10 h-10 rounded-full" />
+                  <span class="ml-2"> {{ message.author.nickname || message.author.name }}</span>
                 </div>
-            </div>
+                <span class="text-gray-500 text-sm">{{ new Date(message.timestamp).toLocaleString()
+                }}</span>
+              </div>
+            </a>
+            <ContentRendererMarkdown :value="message.parsedContent" />
+          </div>
         </div>
-    </Container>
+      </div>
+    </div>
+  </Container>
 </template>
 
 <script>
 
-import forumThread from '~/static/forumThread.json';
 import markdownParser from '@nuxt/content/transformers/markdown'
+const getThread = (id) => import(`../../static/${id}.json`).then(m => m.default || m)
 
 
 export default {
-    data() {
-        return {
-            thread: forumThread,
-            parsedMessages: []
-        };
+  async setup() {
+    const route = useRoute()
+    const thread = await getThread(route.params.thread)
+    return {
+      thread,
+      parsedMessages: await Promise.all(
+        thread.messages.map(async message => {
+          message.parsedContent = await markdownParser.parse(message.id, message.content);
+          return message;
+        })
+      )
+    };
+  },
+  methods: {
+    formatContent(content) {
+      // Convert markdown links to anchor tags, you can expand this for other markdown features
+      return content.replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2" target="_blank">$1</a>');
     },
-    async created() {
-        this.parsedMessages = await Promise.all(
-            this.thread.messages.map(async message => {
-                message.parsedContent = await markdownParser.parse(message.id, message.content);
-                return message;
-            })
-        );
-    },
-    methods: {
-        formatContent(content) {
-            // Convert markdown links to anchor tags, you can expand this for other markdown features
-            return content.replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2" target="_blank">$1</a>');
-        }
-    }
+  },
 };
 </script>
 
--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <Container>
-        <a href="/forum/123">forum thread</a>
-    </Container>
+  <Container>
+    <a v-for="thread in threadsMeta" :key="thread.id" :href="`/forum/${thread.id}`">
+      <div class="bg-gray-100 p-4 rounded-lg mb-6">
+        <h2 class="text-xl font-semibold">{{ thread.title }}</h2>
+      </div>
+    </a>
+  </Container>
 </template>
+
+<script>
+import threadsMeta from '~/static/threads-meta.json'
+export default {
+  data() {
+    return {
+      threadsMeta
+    }
+  }
+}
+</script>
