d5ced13f1209f0639e90085fbbde10aa0aaf09fa
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-06 21:03:18 +0200

    remove column padding

--- app.config.ts
+++ app.config.ts
@@ -63,55 +63,5 @@ footer: {
         color: 'default',
         spinnerComponent: 'DAnimationSpinner'
     }
-<<<<<<< HEAD
-  },
-    contentMermaid: {
-        enabled: true,
-        /**
-     * @default 'default'
-     * @description 'default' or '@nuxtjs/color-mode'
-     */
-        color: 'default',
-        spinnerComponent: 'DAnimationSpinner'
-    }
-=======
->>>>>>> main
 })
 
-// export default defineAppConfig({
-//   docus: {
-//     title: 'Genie Cloud Resource Hub',
-//     description: '',
-//     layout: '',
-//     image: '/meta.png',
-//     url: '',
-//     debug: false,
-//     socials: {
-//       twitter: '@genieMVC',
-//       github: 'genieframework'
-//     },
-//     github: {
-//       root: 'content',
-//     owner: "PGimenez",
-//     repo: "geniedocssite",
-//     branch: "main",
-//       edit: false,
-//       releases: true
-//     },
-//     aside: {
-//       level: 1
-//     },
-//     header: {
-//       title: '',
-//       showLinkIcon: true,
-//       logo: true,
-//     },
-//     footer: {
-//       credits: {
-//         icon: 'IconDocus',
-//         text: 'Powered by Docus',
-//         href: 'https://docus.dev'
-//       },
-//     }
-//   }
-// })
