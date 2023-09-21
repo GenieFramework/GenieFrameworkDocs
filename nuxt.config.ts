64da1dcb041445c0e0562d1a46f3550936856bdc
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-21 11:59:51 +0200

    gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/plausible',
         '@nuxt/devtools',
-        "@nuxtjs/tailwindcss", "nuxt-og-image",
+        "@nuxtjs/tailwindcss", "nuxt-og-image", '@zadigetvoltaire/nuxt-gtm'
     ],
     buildModules: ["@nuxtjs/google-analytics"],
     ssr: true,
@@ -63,9 +63,10 @@ export default defineNuxtConfig({
         preference: "light",
         classSuffix: ""
     },
-    googleAnalytics: {
-        id: 'G-F590JX609F'
-    }
+  gtm: {
+    id: 'GTM-MJLRHNL7', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+    devtools: true, // (optional)
+  }
 })
 
 // import { resolve } from "pathe";
