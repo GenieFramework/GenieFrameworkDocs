dfbb6773d82783505b3311c6256d458ffb7eb1cf
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-22 18:24:57 +0200

    font, colors

--- design.config.ts
+++
@@ -1,8 +0,0 @@
-import { sky, zinc } from 'tailwindcss/colors'
-// const colors = require('tailwindcss/colors');
-
-
-export const primary = sky
-
-export const secondary = zinc
-
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,9 +1,26 @@
 import { defineTheme } from 'pinceau'
-import { primary, secondary } from './design.config'
-
+// export const primary = '#00B0EE'
+// export const secondary = '#000E55'
 export default defineTheme({
-  color: {
-    primary,
-    secondary,
-  },
+    media: {
+        tablet: '(min-width: 768px)',
+        desktop: '(min-width: 1024px)',
+    },
+    // color: {
+    //   primary,
+    //   secondary,
+    // },
+
+    elements:{
+        // backdrop:{background:'#EBF9FD'},
+        text:{secondary:{color:{static:'#687084'}}}},
+    prose:{
+        li:{margin:'8px'},
+        ul:{margin:'8px 0'},
+        p:{
+            margin: '15px 0'
+        }
+    },
+    typography:{font: { body: 'Inter, sans-serif'}},
+
 })
