8f59d078a1a1df161ac689b8431771c3c55fea0e
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-26 21:55:48 +0200

    disable pinceau

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,26 +1,26 @@
-import { defineTheme } from 'pinceau'
-// export const primary = '#00B0EE'
-// export const secondary = '#000E55'
-export default defineTheme({
-    media: {
-        tablet: '(min-width: 768px)',
-        desktop: '(min-width: 1024px)',
-    },
-    // color: {
-    //   primary,
-    //   secondary,
-    // },
-
-    elements:{
-        // backdrop:{background:'#EBF9FD'},
-        text:{secondary:{color:{static:'#687084'}}}},
-    prose:{
-        li:{margin:'8px'},
-        ul:{margin:'8px 0'},
-        p:{
-            margin: '15px 0'
-        }
-    },
-    typography:{font: { body: 'Inter, sans-serif'}},
-
-})
+// import { defineTheme } from 'pinceau'
+// // export const primary = '#00B0EE'
+// // export const secondary = '#000E55'
+// export default defineTheme({
+//     media: {
+//         tablet: '(min-width: 768px)',
+//         desktop: '(min-width: 1024px)',
+//     },
+//     // color: {
+//     //   primary,
+//     //   secondary,
+//     // },
+//
+//     elements:{
+//         // backdrop:{background:'#EBF9FD'},
+//         text:{secondary:{color:{static:'#687084'}}}},
+//     prose:{
+//         li:{margin:'8px'},
+//         ul:{margin:'8px 0'},
+//         p:{
+//             margin: '15px 0'
+//         }
+//     },
+//     typography:{font: { body: 'Inter, sans-serif'}},
+//
+// })
