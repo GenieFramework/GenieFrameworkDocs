9ab10ee87a5f037771098b4b49501e127e328ef4
PGimenez <gimenezfebrer@gmail.com>
2024-01-16 16:31:45 +0100

    logo aspect ratio

--- components/islands/PostOgImage.vue
+++ components/islands/PostOgImage.vue
@@ -24,8 +24,8 @@ const initialStyles = {
             <p :style="{ fontSize: '55px', lineHeight: '1.5em' }" class="font-bold " v-html="title"></p>
             <p :style="{ fontSize: '35px', lineHeight: '1.5em' }" class="" v-html="description"></p>
         </div>
-        <div class="flex items-center flex-row absolute left-25 top-20" :style="{ width: '1000px', left: '85px' }">
-            <img src="/assets/logo.svg" width="160" height="65" class="rounded-xl mr-5">
+        <div class="flex items-center flex-row absolute left-20 top-20" :style="{ width: '1000px', left: '85px' }">
+            <img src="/assets/logo.svg" width="120" height="110" class="rounded-xl mr-5">
         </div>
     </div>
 </template>
