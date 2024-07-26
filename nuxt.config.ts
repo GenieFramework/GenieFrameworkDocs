8874b5503c8dab93d75822791ce19065f1b4da33
PGimenez <gimenezfebrer@gmail.com>
2024-07-26 18:40:06 +0200

    transpile shiki

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,72 +1,73 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
+    extends: ['@nuxt/ui-pro'],
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    'nuxt-og-image',
-    "nuxt-link-checker"
-  ],
+    modules: [
+        '@nuxt/content',
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/image',
+        '@nuxt/ui',
+        '@nuxthq/studio',
+        'nuxt-og-image',
+        "nuxt-link-checker"
+    ],
+    build: { transpile: ["shiki"] },
 
-  content: {
-    documentDriven: true,
-    highlight: {
-      langs: ['html', 'julia'],
-      theme: {
-        default: 'github-light',
-        dark: 'github-dark',
-      }
-    }
-  },
+    content: {
+        documentDriven: true,
+        highlight: {
+            langs: ['html', 'julia'],
+            theme: {
+                default: 'github-light',
+                dark: 'github-dark',
+            }
+        }
+    },
 
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+    hooks: {
+        // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+        'components:extend': (components) => {
+            const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
 
-      globals.forEach(c => c.global = true)
-    }
-  },
+            globals.forEach(c => c.global = true)
+        }
+    },
 
-  ui: {
-    icons: ['heroicons', 'simple-icons']
-  },
+    ui: {
+        icons: ['heroicons', 'simple-icons']
+    },
 
-  colorMode: {
-    disableTransition: true
-  },
+    colorMode: {
+        disableTransition: true
+    },
 
-  routeRules: {
-    // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
-    '/': { prerender: true },
-    '/api/search.json': { prerender: true }
-  },
+    routeRules: {
+        // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
+        '/': { prerender: true },
+        '/api/search.json': { prerender: true }
+    },
 
-  devtools: {
-    enabled: true
-  },
+    devtools: {
+        enabled: true
+    },
 
-  typescript: {
-    strict: false
-  },
+    typescript: {
+        strict: false
+    },
 
-  future: {
-    compatibilityVersion: 4
-  },
+    future: {
+        compatibilityVersion: 4
+    },
 
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  },
+    eslint: {
+        config: {
+            stylistic: {
+                commaDangle: 'never',
+                braceStyle: '1tbs'
+            }
+        }
+    },
 
-  compatibilityDate: '2024-07-11'
+    compatibilityDate: '2024-07-11'
 })
